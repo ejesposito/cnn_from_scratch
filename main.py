a6203b16860cbd30fd4b8db96b83927a0be9f088
Ezequiel Esposito <ejesposito@Ezequiels-MBP.attlocal.net>
2019-04-24 13:13:11 -0700

    Predictor working

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 import scipy.io as sc
 import argparse
 
+from PIL import Image
 import cv2
+
+import torch
 
 from dataset import DataSet
 from trainer import Trainer
-
+from custommodel import CustomModel
+from predictor import Predictor
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-mode', '--mode', default='train')
 parser.add_argument('-m', '--model', default='custom')
 parser.add_argument('-c', '--cuda', default='cuda:1')
 parser.add_argument('-w', '--n_workers', default=0, type=int)
@@ -19,11 +24,12 @@ parser.add_argument('-sm', '--sgd_momentum', default=0.9, type=float)
 parser.add_argument('-sw', '--sgd_weight_decay', default=0.0005, type=float)
 parser.add_argument('-ds', '--decay_steps', default=10000, type=int)
 parser.add_argument('-dr', '--decay_rate', default=0.9, type=float)
+parser.add_argument('-img', '--image')
 
 
 def main(args):
-    # args
     training_options = {
+        'mode': args.mode,
         'model': args.model,
         'cuda': args.cuda,
         'n_workers': args.n_workers,
@@ -33,13 +39,27 @@ def main(args):
         'momentum': args.sgd_momentum,
         'weight_decay': args.sgd_weight_decay,
         'decay_steps': args.decay_steps,
-        'decay_rate': args.decay_rate
+        'decay_rate': args.decay_rate,
+        'image': args.image
     }
-    # load the dataset in memory
-    data_set = DataSet()
-    # start train
-    trainer = Trainer(data_set.train, data_set.test, training_options)
-    trainer.fit()
+
+    if training_options['mode'] == 'train':
+        # load the dataset in memory
+        data_set = DataSet()
+        # start train
+        trainer = Trainer(data_set.train, data_set.test, training_options)
+        trainer.fit()
+    else:
+        # load the model to compute predictions
+        if training_options['model'] == 'custom':
+            model = CustomModel()
+            model.load_state_dict(torch.load(os.path.join('results', 'custom_model.pth'), map_location='cpu'))
+        # load the images
+        image = Image.open(os.path.join('test_assets', training_options['image']))
+        #image.show()
+        # compute the predictions
+        predictor = Predictor(model, training_options['cuda'])
+        predictor.predict(image)
 
 
 if __name__ == '__main__':
--- predictor.py
+++ predictor.py
@@ -1,63 +1,38 @@
-import os
-from pathlib import Path
-from PIL import Image
+import argparse
 import torch
-import torch.utils.data
-from torchvision import transforms
 
-from dataset import TorchDataSet
+from PIL import Image
+from torchvision import transforms
 
 
 class Predictor(object):
-    def __init__(self, test, cuda):
-        transform = transforms.Compose([
-            transforms.CenterCrop([54, 54]),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-        ])
-        self._loader = torch.utils.data.DataLoader(TorchDataSet(test['image'],
-                                                                test[['number_digits', 'd1', 'd2', 'd3', 'd4']],
-                                                                transform),
-                                                   batch_size=128, shuffle=False)
-        self.transform = transform
+
+    def __init__(self, model, cuda):
+        # model to gpu if it's available
         self.device = torch.device('cpu')
         if torch.cuda.is_available():
             self.device = torch.device(cuda)
+        self.model = model.to(self.device)
 
-    def evaluate(self, model):
-        num_correct = 0
-
+    def predict(self, image):
         with torch.no_grad():
-            for batch_idx, (images, length_labels, digits_labels) in enumerate(self._loader):
-                images = images.to(self.device)
-                length_labels = length_labels.to(self.device)
-                digits_labels = [digit_labels.to(self.device) for digit_labels in digits_labels]
-                length_logits, digit1_logits, digit2_logits, digit3_logits, digit4_logits = model.eval()(images)
+            transform = transforms.Compose([
+                transforms.Resize([64, 64]),
+                transforms.CenterCrop([54, 54]),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ])
 
-                length_prediction = length_logits.max(1)[1]
-                digit1_prediction = digit1_logits.max(1)[1]
-                digit2_prediction = digit2_logits.max(1)[1]
-                digit3_prediction = digit3_logits.max(1)[1]
-                digit4_prediction = digit4_logits.max(1)[1]
+            image = transform(image)
+            images = image.unsqueeze(dim=0).to(self.device)
 
-                num_correct += (digit1_prediction.eq(digits_labels[0]) &
-                                digit2_prediction.eq(digits_labels[1]) &
-                                digit3_prediction.eq(digits_labels[2]) &
-                                digit4_prediction.eq(digits_labels[3])).cpu().sum()
+            length_logits, digit1_logits, digit2_logits, digit3_logits, digit4_logits = self.model.eval()(images)
 
-        accuracy = num_correct.item() / len(self._loader.dataset)
-        image = Image.open(os.path.join('eval', '1035_1.jpg'))
-        image = self.transform(image)
-        images = image.unsqueeze(dim=0).to(self.device)
-        length_logits, digit1_logits, digit2_logits, digit3_logits, digit4_logits = model.eval()(images)
+            length_prediction = length_logits.max(1)[1]
+            digit1_prediction = digit1_logits.max(1)[1]
+            digit2_prediction = digit2_logits.max(1)[1]
+            digit3_prediction = digit3_logits.max(1)[1]
+            digit4_prediction = digit4_logits.max(1)[1]
 
-        length_prediction = length_logits.max(1)[1]
-        digit1_prediction = digit1_logits.max(1)[1]
-        digit2_prediction = digit2_logits.max(1)[1]
-        digit3_prediction = digit3_logits.max(1)[1]
-        digit4_prediction = digit4_logits.max(1)[1]
-
-        print('length:', length_prediction.item())
-        print('digits:', digit1_prediction.item(), digit2_prediction.item(), digit3_prediction.item(), digit4_prediction.item())
-
-        return accuracy
+            print('length:', length_prediction.item())
+            print('digits:', digit1_prediction.item(), digit2_prediction.item(), digit3_prediction.item(), digit4_prediction.item())
